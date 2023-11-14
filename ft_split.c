99f05fa8aba74892a197bde5c216d6d01070fedc
youssef <youssefjablaoui2@gmail.com>
2023-11-14 08:09:24 +0100

    youssef1

--- ft_split.c
+++ ft_split.c
@@ -2,82 +2,71 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-size_t find(const char *s, char c, int **l)
-{
+size_t find(const char *s, char c, int **l) {
     size_t k = 0;
     size_t i = 0;
     size_t p = 0;
     *l = (int *)malloc((ft_strlen(s) + 1) * sizeof(int));
-    if (!*l)
-        return 0;
-    
-    while (s[p])
-    {
-        if (s[p] == c)
-        {
+    if (!*l) return 0;
+
+    while (s[p]) {
+        if (s[p] == c) {
             (*l)[k++] = p;
             i++;
         }
         p++;
     }
-    (*l)[k] = p; // Store the end position at index k
-    return (i + 1); // Return the count of delimiters + 1 (for the end position)
+    (*l)[k] = -1; // Use -1 to mark the end of positions instead of the string length
+    return (i);   // Return the count of delimiters
 }
 
-char **ft_split(char const *s, char c)
-{
+char **ft_split(char const *s, char c) {
     size_t i;
     size_t p;
     char **str;
     int *l;
 
-    p = 0;
-    l = NULL;
+    if (!s || !(l = (int *)malloc((ft_strlen(s) + 1) * sizeof(int))))
+        return (NULL);
+
     i = find(s, c, &l);
-    if (!s || !(str = (char **)malloc((i + 1) * sizeof(char *))) || !l)
-    {
-        if (l)
-            free(l);
+    if (!(str = (char **)malloc((i + 1) * sizeof(char *)))) {
+        free(l);
         return (NULL);
     }
-    while (p < i)
-    {
-        str[p] = ft_substr(s, (unsigned int)l[p] + 1, l[p + 1] - l[p] - 1);
-        if (str[p] == NULL)
-        {
-            while (p > 0)
-            {
-                free(str[p]);
-                p--;
-            }
+
+    p = 0;
+    i = 0;
+    while (l[i] != -1) {
+        str[i] = ft_substr(s, p, l[i] - p);
+        if (str[i] == NULL) {
+            while (i > 0)
+                free(str[--i]);
             free(str);
             free(l);
             return (NULL);
         }
-        p++;
+        p = l[i++] + 1;
     }
+    str[i] = NULL;
     free(l);
-    str[p] = NULL;
     return (str);
 }
 
-int main()
-{
+int main() {
     char **result;
-    char str[] = "This is a test string";
+    char str[] = "This is a test string jbjbk jbjkbjb kjkjbnkjn jkbkjbkub hjbujbub uibkub biubjkbinb kibikbuk buknb jkb jkbn ionkb jkbuk bjkbkj ";
     char sep = ' ';
 
     result = ft_split(str, sep);
 
-    if (result == NULL)
-    {
+    if (result == NULL) {
         printf("Memory allocation error or input string is empty.\n");
         return 1;
     }
 
     // Print the resulting strings
-    for (int i = 0; result[i] != NULL; i++)
-    {
+    for (int i = 0; result[i] != NULL; i++) {
         printf("Token %d: %s\n", i + 1, result[i]);
         free(result[i]); // Free each string
     }
